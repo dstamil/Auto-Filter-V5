bb91d252a4011462118801ad03337583fd2dd38a
dstamil <99588972+dstamil@users.noreply.github.com>
2022-12-20 17:02:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', -1001888886560))
 FILE_CHANNEL = int(environ.get('FILE_CHANNEL', -1001807703129))
 FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 't.me/channeegj')
-HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/cynitemovies/3')
+HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/how_to_downloadk/29')
 
 # Log Channels
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001819673897))
